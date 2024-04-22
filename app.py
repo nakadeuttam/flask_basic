a3b5414b11bb739c71dcc467b2152864d28ad600
nakadeuttam <nakadeuttam66@gmail.com>
2024-04-22 13:54:45 +0530

    m

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World!'
+    return 'Hey this is my first flask project i initialized'
 
 if __name__ == '__main__':
     app.run(debug=True)
